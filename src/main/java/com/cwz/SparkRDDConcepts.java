dcd55c6bc61bc289252584c14cf03a94269370dc
Zartab Nakhwa <zartab@codewithz.org>
2024-09-13 06:49:09 +0530

    Added Scenario 5 to SparkRDDConcepts

--- src/main/java/com/cwz/SparkRDDConcepts.java
+++ src/main/java/com/cwz/SparkRDDConcepts.java
@@ -90,47 +90,84 @@ public class SparkRDDConcepts {
 //        //        Identify transformations-3 [N], actions-1, jobs-1,stage-1 ,partitions-4, tasks-4
 
 
-        System.out.println("------------------------------Scenario 4------------------------------------");
+//        System.out.println("------------------------------Scenario 4------------------------------------");
+//
+//        String filePath="C:\\Spark\\DataFiles\\TaxiZones.csv";
+//
+//        JavaRDD<String> taxiZonesRdd=context.textFile(filePath,4);
+//
+//        int partitionsAfterReading=taxiZonesRdd.getNumPartitions();
+//        System.out.println("After Reading the file: "+partitionsAfterReading);
+//
+//        JavaRDD<String[]> taxiZonesWithColsRDD=taxiZonesRdd.map(
+//                line -> line.split(",")
+//        );
+//
+//        int partitionsAfterMapping=taxiZonesWithColsRDD.getNumPartitions();
+//        System.out.println("After Applying Map: "+partitionsAfterMapping);
+//
+//        JavaPairRDD<String , Integer> taxiZonesPairRDD=taxiZonesWithColsRDD.mapToPair(
+//                zoneRow -> new Tuple2<>(zoneRow[1],1)  //Key : Borough, Value :1
+//        );
+//
+//        int partitionsAfterMappingToPairRDD=taxiZonesPairRDD.getNumPartitions();
+//        System.out.println("After Applying Map to Pair RDD: "+partitionsAfterMappingToPairRDD);
+//
+//        JavaPairRDD<String,Integer> distinctZonesRdd=taxiZonesPairRDD
+//                .distinct();
+//
+//        int partitionsAfterDistinctToPairRDD=distinctZonesRdd.getNumPartitions();
+//        System.out.println("After Applying Distinct to Pair RDD: "+partitionsAfterDistinctToPairRDD);
+//
+//        List<Tuple2<String,Integer>> distinctResults=distinctZonesRdd
+//                .collect();
+//        System.out.println("Disitinct Boroughs");
+//        for(Tuple2<String,Integer> result:distinctResults){
+//            System.out.println("Borough:"+result._1+" | Value : "+result._2);
+//        }
+//
+//
+
+
+        //        Identify transformations-4  [N-3,W-1], actions-1, jobs-1,stage-2 ,partitions-4, tasks-4
+
+
+
+        System.out.println("------------------------------Scenario 5------------------------------------");
 
         String filePath="C:\\Spark\\DataFiles\\TaxiZones.csv";
 
         JavaRDD<String> taxiZonesRdd=context.textFile(filePath,4);
 
-        int partitionsAfterReading=taxiZonesRdd.getNumPartitions();
-        System.out.println("After Reading the file: "+partitionsAfterReading);
-
         JavaRDD<String[]> taxiZonesWithColsRDD=taxiZonesRdd.map(
                 line -> line.split(",")
         );
-
-        int partitionsAfterMapping=taxiZonesWithColsRDD.getNumPartitions();
-        System.out.println("After Applying Map: "+partitionsAfterMapping);
-
         JavaPairRDD<String , Integer> taxiZonesPairRDD=taxiZonesWithColsRDD.mapToPair(
                 zoneRow -> new Tuple2<>(zoneRow[1],1)  //Key : Borough, Value :1
         );
 
-        int partitionsAfterMappingToPairRDD=taxiZonesPairRDD.getNumPartitions();
-        System.out.println("After Applying Map to Pair RDD: "+partitionsAfterMappingToPairRDD);
+        // Distinct Operation - Find distinct items
+        JavaPairRDD<String, Integer> distinctZonesRdd = taxiZonesPairRDD.distinct();
 
-        JavaPairRDD<String,Integer> distinctZonesRdd=taxiZonesPairRDD
-                .distinct();
+        // ReduceByKey Operation - Group by Borough and calculate count
+        JavaPairRDD<String, Integer> boroughCountRdd = distinctZonesRdd.reduceByKey(
+                (accumulator, currentValue) -> accumulator + currentValue
+        );
 
-        int partitionsAfterDistinctToPairRDD=distinctZonesRdd.getNumPartitions();
-        System.out.println("After Applying Distinct to Pair RDD: "+partitionsAfterDistinctToPairRDD);
-
-        List<Tuple2<String,Integer>> distinctResults=distinctZonesRdd
-                .collect();
-        System.out.println("Disitinct Boroughs");
-        for(Tuple2<String,Integer> result:distinctResults){
-            System.out.println("Borough:"+result._1+" | Value : "+result._2);
+        // Filter Operation - Get rows with Borough count > 10
+        JavaPairRDD<String, Integer> filteredZonesRdd = boroughCountRdd.filter(
+                row -> row._2 > 10  // Filtering rows with count > 10
+        );
+        // Collect and print the results
+        List<Tuple2<String, Integer>> result = filteredZonesRdd.collect();
+        System.out.println("Boroughs with count > 10:");
+        for (Tuple2<String, Integer> row : result) {
+            System.out.println("Borough: " + row._1 + ", Count: " + row._2);
         }
 
 
 
-
-        //        Identify transformations-4  [N-3,W-1], actions-1, jobs-1,stage-2 ,partitions-4, tasks-4
-
+        //        Identify transformations-6  [N-4,W-2], actions-1, jobs-1,stage-3 ,partitions-4, tasks-4
 
 
 
